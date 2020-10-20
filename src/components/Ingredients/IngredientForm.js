83a31d26d87ab71019a6b33efe6e9acbd96827c9
ozvaris <ozgurozvaris@hotmail.com>
2020-10-20 20:12:57 +0300

    Error handling

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 
 import Card from '../UI/Card';
+import LoadingIndicator from '../UI/LoadingIndicator';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
@@ -11,6 +12,7 @@ const IngredientForm = React.memo(props => {
     props.onAddIngredient({title: enteredTitle,  amount: enteredAmount})
   };
 
+  
   return (
     <section className="ingredient-form">
       <Card>
@@ -37,6 +39,7 @@ const IngredientForm = React.memo(props => {
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
+            {props.loading && <LoadingIndicator />}
           </div>
         </form>
       </Card>
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -2,11 +2,14 @@ import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
+import ErrorModal from '../UI/ErrorModal';
 import Search from './Search';
 
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
-  
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
   useEffect(() => {
     console.log('RENDERING INGREDIENTS', userIngredients);
   }, [userIngredients]);
@@ -15,8 +18,9 @@ function Ingredients() {
     setUserIngredients(filteredIngredients)
   }, []);
 
-  
+
   const addIngredientHandler = ingredient => {
+    setIsLoading(true);
     fetch('https://reacthook-8c62f.firebaseio.com/ingredients.json', {
       method: 'POST',
       body: JSON.stringify(ingredient),
@@ -26,6 +30,7 @@ function Ingredients() {
         return response.json();
       })
       .then(responseData => {
+        setIsLoading(false);
         setUserIngredients(prevIngredients => [
           ...prevIngredients,
           { id: responseData.name, ...ingredient }
@@ -34,20 +39,33 @@ function Ingredients() {
   };
 
   const removeIngredientHandler = ingredientId => {
+    setIsLoading(true);
     fetch(`https://reacthook-8c62f.firebaseio.com/ingredients/${ingredientId}.json`, {
       method: 'DELETE',
-    })
-      .then(responseData => {
-        setUserIngredients( prevIngredients => 
-          prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
-        );
-      })
-    
+    }).then(responseData => {
+      setIsLoading(false);
+      setUserIngredients(prevIngredients =>
+        prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
+      );
+    }).catch(error => {
+      setError('Something went wrong!');
+      setIsLoading(false);
+    });
+
   };
+
+  const clearError = () => {
+    setError(null);
+  }
+
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler}/>
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      
+      <IngredientForm 
+        onAddIngredient={addIngredientHandler}
+        loading={isLoading}/>
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler}/>
