7b16207839f4887006052389af2e43d201d31b2e
ozvaris <ozgurozvaris@hotmail.com>
2020-10-20 12:09:26 +0300

    useEffect

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -6,6 +6,25 @@ import Search from './Search';
 
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
+  useEffect(() => {
+    fetch('https://reacthook-8c62f.firebaseio.com/ingredients.json')
+    .then(response => response.json())
+    .then(responseData => {
+      const loadedIngredients = [];
+      for (const key in responseData){
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount
+        });
+      }
+      setUserIngredients(loadedIngredients);
+    })
+  }, [])
+
+  useEffect(() => {
+    console.log('RENDERING INGREDIENTS', userIngredients);
+  }, [userIngredients]);
 
   const addIngredientHandler = ingredient => {
     fetch('https://reacthook-8c62f.firebaseio.com/ingredients.json', {
